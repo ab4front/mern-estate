b29552ee54eb942f85d5108a28359916399d806e
Oleksandr Borysenko <3pc@ukr.net>
2024-04-21 15:55:20 +0300

    add test route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js";
 dotenv.config();
 
 mongoose
@@ -14,6 +15,8 @@ mongoose
 
 const app = express();
 
-app.listen(3333, () => {
-  console.log("Server started on port 3000!");
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Server started on port ${process.env.APP_PORT}!`);
 });
+
+app.use(`${process.env.API_PREFIX}/user`, userRouter);
